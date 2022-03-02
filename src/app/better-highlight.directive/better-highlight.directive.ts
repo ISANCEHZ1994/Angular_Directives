be0e00561af77ca07436a87e4d97f8c152bad986
Israel Sanchez <Israel.pro1994@gmail.com>
2022-03-02 13:26:45 -0600

    fixed the strict and also learned about Host Binding

--- src/app/better-highlight.directive/better-highlight.directive.ts
+++ src/app/better-highlight.directive/better-highlight.directive.ts
@@ -1,4 +1,11 @@
-import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { 
+  Directive, 
+  ElementRef,
+  HostBinding, 
+  HostListener, 
+  OnInit, 
+  Renderer2 
+} from '@angular/core';
 
   // why is it better? 
   // Angular is not limited to running in the browser 
@@ -10,25 +17,30 @@ import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
   
   constructor( private elRef: ElementRef, private renderer: Renderer2 ) {};
 
   ngOnInit() {
     // look into the function setStyle!   
+    // function moved to HostListeners below
     // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
   };
 
   // this can be triggered whenever some event occurs - LISTENS to events on element
   // HostListener: takes in the argument name as an input
   @HostListener('mouseenter') mouseover(eventData: Event){
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    this.backgroundColor = 'blue';
   };
 
   @HostListener('mouseleave') mouseleave(eventData: Event){
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+    this.backgroundColor = 'transparent';
   };
 
-  
+  // Now when you hover on the div the blue background will appear and leave will change to transparent!
 
 };
 
