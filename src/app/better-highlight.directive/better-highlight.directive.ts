cde6a520fbb20fac888ab227f803e7d79087432f
Israel Sanchez <Israel.pro1994@gmail.com>
2022-03-02 11:40:16 -0600

    HostListener under work

--- src/app/better-highlight.directive/better-highlight.directive.ts
+++ src/app/better-highlight.directive/better-highlight.directive.ts
@@ -1,16 +1,4 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
-
-@Directive({
-  selector: '[appBetterHighlight]'
-})
-export class BetterHighlightDirective implements OnInit {
-
-  constructor( private elRef: ElementRef, private renderer: Renderer2 ) { }
-
-  ngOnInit() {
-    // setStyle can take in 4 arguments - the last one is optional known as a flag..     
-      this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
-  }
+import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
 
   // why is it better? 
   // Angular is not limited to running in the browser 
@@ -18,4 +6,29 @@ export class BetterHighlightDirective implements OnInit {
   // you are more likely to get an error in some circumstance
   // literally depends however this way 'better' is good pracitce
 
-}
+@Directive({
+  selector: '[appBetterHighlight]'
+})
+export class BetterHighlightDirective implements OnInit {
+  
+  constructor( private elRef: ElementRef, private renderer: Renderer2 ) {};
+
+  ngOnInit() {
+    // look into the function setStyle!   
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+  };
+
+  // this can be triggered whenever some event occurs - LISTENS to events on element
+  // HostListener: takes in the argument name as an input
+  @HostListener('mouseenter') mouseover(eventData: Event){
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+  };
+
+  @HostListener('mouseleave') mouseleave(eventData: Event){
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+  };
+
+  
+
+};
+
